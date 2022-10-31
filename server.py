a1b75c957e4909e7724b4816d6eb8afcf84f2a0e
4L3PHZ3R0 <114233958+4L3PHZ3R0@users.noreply.github.com>
2022-10-31 10:44:39 +0200

    Add files via upload
    working server

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ import time
 from threading import Thread
 from hashlib import md5
 
-
+#10.30.56.225
+#25565
 def get_prefix(length):
     prefix = "a" * length
     while prefix != "z"*length:
@@ -12,13 +13,13 @@ def get_prefix(length):
             prefix = prefix[:-1] + "a"
             for i in range(length-2, -1, -1):
                 if prefix[i] == "z":
-                    prefix = prefix[:i] + "a" + + prefix[i+1:]
+                    prefix = prefix[:i] + "a" + prefix[i+1:]
                 else:
-                    prefix = prefix[:i] + \
-                        chr(ord(prefix[i]) + 1) + prefix[i+1:]
+                    prefix = prefix[:i] + chr(ord(prefix[i]) + 1) + prefix[i+1:]
                     break
         else:
             prefix = prefix[:-1]+chr(ord(prefix[-1]) + 1)
+    yield prefix
 
 class Server:
     def __init__(self, md5_hash, client_length=5, total_length=8):
@@ -33,7 +34,7 @@ class Server:
     def mainloop(self):
         server_soc = socket()
         server_soc.setblocking(False)
-        server_soc.bind(('127.0.0.1', 25565))
+        server_soc.bind(('0.0.0.0', 25565))
         server_soc.listen(2)
         connections = []
         print("Server started")
@@ -41,24 +42,27 @@ class Server:
             try:
                 cli_soc, addr = server_soc.accept()
                 connections.append(cli_soc)
-                Thread(target=self.handle_client, args=(cli_soc,)).start()
+                Thread(target=self.handle_client, args=(cli_soc, addr)).start()
             except:
                 pass
 
         for soc in connections:
-            soc.send("end".encode())
+            try:
+                soc.send("end".encode())
+            except:
+                pass
 
-        time.sleep(0.001)
+        time.sleep(0.01)
         for soc in connections:
             soc.close()
 
         server_soc.close()
         return self.passwd
 
-    def handle_client(self, conn):
-        conn.settimeout(30.0)
+    def handle_client(self, conn ,addr):
+        conn.settimeout(120.0)
         conn.setblocking(True)
-        print("Client connected")
+        print(f"Client {addr} connected")
         msg = conn.recv(4096).decode()
         cur_work = ""
 
@@ -70,13 +74,17 @@ class Server:
             try:
                 msg = conn.recv(4096).decode()
                 if msg == "ready":
-                    print("Client ready")
-                    if self.queue == []:
-                        self.queue.append(next(self.prefix_gen))
+                    print(f"Client {addr} ready")
+                    while self.queue == []:
+                        try:
+                            self.queue.append(next(self.prefix_gen))
+                        except:
+                            time.sleep(0.001)
 
                     cur_work = self.queue.pop()
-                    conn.send(
-                        f'{self.md5_hash},{cur_work},{self.client_length}'.encode())
+                    msg = f'{self.md5_hash},{cur_work},{self.client_length}'
+                    print(f'sent {addr} msg: {msg}')
+                    conn.send(msg.encode())
 
                 elif msg[:5] == "found":
                     passwd = msg[6:]
@@ -84,13 +92,15 @@ class Server:
                         self.passwd = passwd
                         self.found = True
                         break
+                        
             except:
+                print(f"connection {addr} closed")
                 conn.close()
                 self.queue.append(cur_work)
                 break
 
 def main():
-    server = Server("900150983cd24fb0d6963f7d28e17f72")
+    server = Server(str(md5("bssswd".encode()).hexdigest()), 4, 6)
     print(server.mainloop())
 
 if __name__ == '__main__':
