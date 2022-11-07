7c96290440ee84730163fbc9f9185acd9a72492e
4L3PHZ3R0 <114233958+4L3PHZ3R0@users.noreply.github.com>
2022-11-07 09:56:35 +0200

    Updated version

---
+++ md5 client.py
@@ -0,0 +1,87 @@
+from concurrent.futures import thread
+from errno import ENOMSG
+import socket
+from hashlib import md5
+from threading import Thread
+
+FOUND = False
+ANSWER = ''
+data = None
+s = None
+
+def ServerConnect():
+    global FOUND, data
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('10.30.58.40', 25565)
+    s.connect(server_address)
+    print("Connected")
+    s.send("Hello".encode())
+    ok = s.recv(1024).decode()
+    assert ok == 'ok'
+    s.send("ready".encode())
+    data = s.recv(1024).decode()
+    threadEnd = Thread(target = listenForEnd , args = (s,))
+    threadEnd.start()
+    while not FOUND:
+        print("ready")
+        hash,prefix,num = data.split(',')
+        num = int(num)
+        threads = []
+        for i in range(26):
+            thread = Thread(target = engine , args = (hash,prefix + chr(97+i),num -1))
+            thread.start()
+            threads.append(thread)
+        for thread in threads:
+            thread.join()
+        if FOUND:
+            print("found",ANSWER)
+        else:
+            print("Cant Found!")
+            old_data = data
+            s.sendall(b"ready")
+            print("weeeee")
+            while old_data == data:
+                pass
+        print(FOUND)
+            
+
+def listenForEnd(s):
+    global data, FOUND
+    while not FOUND:
+        temp = s.recv(1024).decode()
+        if temp == "end":
+            FOUND = True
+        else:
+            data = temp
+
+def HashChecking(hash,prefix,str):
+    str = prefix + "".join(str)
+    return md5(str.encode()).hexdigest() == hash
+
+def engine(hash,prefix,num):
+    global FOUND, ANSWER, s
+    start = ['a']*num
+    while not FOUND:
+        if HashChecking(hash, prefix,start):
+            FOUND = True
+            ANSWER = prefix + "".join(start)
+            s.sendall(f"found, {ANSWER}".encode())
+        start = StrUp(start)
+        if start == ['a'] * num: return FOUND
+    
+    
+def StrUp(str):
+    for i in range(len(str)):
+        if str[-1-i] =='z':
+            str[-1-i] = 'a'
+        else:
+            str[-1-i] = chr(ord(str[-1-i])+1)
+            break
+    return str
+
+
+def main():
+    ServerConnect()
+    
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -2,31 +2,32 @@ from socket import *
 import time
 from threading import Thread
 from hashlib import md5
-
-#10.30.56.225
-#25565
+from pygame import mixer
+# 10.30.56.225
+# 25565
 def get_prefix(length):
     prefix = "a" * length
-    while prefix != "z"*length:
+    while prefix != "z" * length:
         yield prefix
         if prefix[-1] == "z":
             prefix = prefix[:-1] + "a"
-            for i in range(length-2, -1, -1):
+            for i in range(length - 2, -1, -1):
                 if prefix[i] == "z":
-                    prefix = prefix[:i] + "a" + prefix[i+1:]
+                    prefix = prefix[:i] + "a" + prefix[i + 1:]
                 else:
-                    prefix = prefix[:i] + chr(ord(prefix[i]) + 1) + prefix[i+1:]
+                    prefix = prefix[:i] + chr(ord(prefix[i]) + 1) + prefix[i + 1:]
                     break
         else:
-            prefix = prefix[:-1]+chr(ord(prefix[-1]) + 1)
+            prefix = prefix[:-1] + chr(ord(prefix[-1]) + 1)
     yield prefix
+
 
 class Server:
     def __init__(self, md5_hash, client_length=5, total_length=8):
         self.queue = []
         self.client_length = client_length
         self.total_length = total_length
-        self.prefix_gen = get_prefix(self.total_length-self.client_length)
+        self.prefix_gen = get_prefix(self.total_length - self.client_length)
         self.md5_hash = md5_hash
         self.found = False
         self.passwd = None
@@ -52,29 +53,44 @@ class Server:
             except:
                 pass
 
-        time.sleep(0.01)
+        t = Thread(target = self.playsound)
+        t.start()
+        time.sleep(0.001)
         for soc in connections:
             soc.close()
 
         server_soc.close()
         return self.passwd
 
-    def handle_client(self, conn ,addr):
-        conn.settimeout(120.0)
+    def playsound(self):
+        mixer.init()
+        mixer.music.load('ole.mp3')
+        mixer.music.set_volume(1)
+        mixer.music.play()
+        time.sleep(47)
+
+    def handle_client(self, conn, addr):
+        conn.settimeout(9999.0)
         conn.setblocking(True)
-        print(f"Client {addr} connected")
+        print("Client {addr} connected".format(addr = addr))
         msg = conn.recv(4096).decode()
         cur_work = ""
 
         while msg != "Hello":
-            msg = conn.recv(4096).decode()
+            try:
+                msg = conn.recv(4096).decode()
+            except:
+                print("connection {addr} closed".format(addr=addr))
+                conn.close()
+                return
+
 
         conn.send("ok".encode())
         while True:
             try:
                 msg = conn.recv(4096).decode()
                 if msg == "ready":
-                    print(f"Client {addr} ready")
+                    print("Client {addr} ready".format(addr=addr))
                     while self.queue == []:
                         try:
                             self.queue.append(next(self.prefix_gen))
@@ -82,8 +98,8 @@ class Server:
                             time.sleep(0.001)
 
                     cur_work = self.queue.pop()
-                    msg = f'{self.md5_hash},{cur_work},{self.client_length}'
-                    print(f'sent {addr} msg: {msg}')
+                    msg = '{0},{1},{2}'.format(self.md5_hash, cur_work, self.client_length)
+                    print('sent {addr} msg: {msg}'.format(addr=addr, msg=msg))
                     conn.send(msg.encode())
 
                 elif msg[:5] == "found":
@@ -92,16 +108,18 @@ class Server:
                         self.passwd = passwd
                         self.found = True
                         break
-                        
+
             except:
-                print(f"connection {addr} closed")
+                print("connection {addr} closed".format(addr=addr))
                 conn.close()
                 self.queue.append(cur_work)
                 break
 
+
 def main():
-    server = Server(str(md5("bssswd".encode()).hexdigest()), 4, 6)
+    server = Server(str(md5("catwalk".encode()).hexdigest()), 5, 7)
     print(server.mainloop())
+
 
 if __name__ == '__main__':
     main()
